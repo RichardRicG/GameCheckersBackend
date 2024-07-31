4b174b55de0cce8864f842a8b75e08cdb2df444a
MutaiNewton56 <mutainewton56.gmail.com>
2024-07-31 17:19:11 +0300

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/homepage',methods=['GET'])
+@app.route('/',methods=['GET'])
 def home_page():
     return 'welcome to checkers game'
 
