0aca21a51bd29e2e58008dbc1f5881abaa3839dd
Randa_Mike <michaelranda95@gmail.com>
2024-08-16 05:46:34 +0300

    updated the db

---
+++ server/migrations/versions/aa9b302825a7_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: aa9b302825a7
+Revises: 911156ed374c
+Create Date: 2024-08-16 04:25:33.750844
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aa9b302825a7'
+down_revision = '911156ed374c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
